17004106515e5067f1499659326b38e1a0e65a2f
brchiru <39872659+brchiru@users.noreply.github.com>
2024-07-11 13:11:39 +0100

    Update 05dashcallback.py
    Testing from Git to pull in to VSCode

--- 05dashcallback.py
+++ 05dashcallback.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input,Output
-
+#app start
 app = dash.Dash()
 
 app.layout = html.Div([
